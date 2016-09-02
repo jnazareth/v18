6a1921a4a4af1e0019174a453704fa9f38ffa62e
jnazareth <U8005108@ten.thomsonreuters.com>
2016-09-02 09:11:45 -0400

    baselined to v17
    removed v17 change desc.

--- gp2.java
+++ gp2.java
@@ -4,7 +4,6 @@
 -------------------
 history
 -------------------
-v17: group implementation
 v16: cleanup: created separete account, removed static declarations
 v15: added category & vendor
 v14: bug fix individual transaction amt
